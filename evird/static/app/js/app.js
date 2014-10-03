ee1636078503f2797af56d7dc5450a64f716e56e
Praveen Gollakota <praveen@chartio.com>
2014-10-02 23:43:08 -0700

    Stub out adding a google auth form in react

--- evird/static/app/js/app.js
+++ evird/static/app/js/app.js
@@ -1,9 +1,20 @@
 /** @jsx React.DOM */
 
 var GoogleApiAuthForm = React.createClass({
-    render: function() {
-        return (<input />);
+    render: function () {
+        return (
+            <form role="form">
+                <div class="form-group">
+                    <label for="client_id">Client Id</label>
+                    <input id="client_id" type="text" name="client_id" placeholder="Google API Client ID"/>
+                </div>
+                <div class="form-group">
+                    <label for="api_key">API Key</label>
+                    <input id="api_key" type="text" name="api_key" placeholder="Google API Key"/>
+                </div>
+            </form>
+        );
     }
 });
 
-React.renderComponent(GoogleApiAuthForm, )
+React.renderComponent(<GoogleApiAuthForm />, document.getElementById('app'));
