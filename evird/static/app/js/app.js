3304d3c286e604358094d04eb5c26a966b824656
Praveen Gollakota <praveen@chartio.com>
2014-10-03 00:05:10 -0700

    Styling

--- evird/static/app/js/app.js
+++ evird/static/app/js/app.js
@@ -4,14 +4,25 @@ var GoogleApiAuthForm = React.createClass({
     render: function () {
         return (
             <form role="form">
-                <div class="form-group">
+                <div className="form-group">
                     <label for="client_id">Client Id</label>
-                    <input id="client_id" type="text" name="client_id" placeholder="Google API Client ID"/>
+                    <input
+                        id="client_id"
+                        className="form-control"
+                        type="text"
+                        name="client_id"
+                        placeholder="Google API Client ID" />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <label for="api_key">API Key</label>
-                    <input id="api_key" type="text" name="api_key" placeholder="Google API Key"/>
+                    <input
+                        id="api_key"
+                        className="form-control"
+                        type="text"
+                        name="api_key"
+                        placeholder="Google API Key"/>
                 </div>
+                <button className="btn btn-lg btn-primary" type="submit">Authenticate</button>
             </form>
         );
     }
