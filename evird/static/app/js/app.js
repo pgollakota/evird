7e0f8470f10a820cdd5d6babc9ea63b854bd38c8
Praveen Gollakota <praveen@chartio.com>
2014-10-04 23:45:56 -0700

    Handle google api client authentication

--- evird/static/app/js/app.js
+++ evird/static/app/js/app.js
@@ -1,12 +1,16 @@
 /** @jsx React.DOM */
 
 var GoogleApiAuthForm = React.createClass({
+    getInitialState: function() {
+        return {apiKey: null, clientId: null};
+    },
     render: function () {
         return (
             <form role="form">
                 <div className="form-group">
                     <label for="client_id">Client Id</label>
                     <input
+                        ref="clientId"
                         id="client_id"
                         className="form-control"
                         type="text"
@@ -16,15 +20,35 @@ var GoogleApiAuthForm = React.createClass({
                 <div className="form-group">
                     <label for="api_key">API Key</label>
                     <input
+                        ref="apiKey"
                         id="api_key"
                         className="form-control"
                         type="text"
                         name="api_key"
                         placeholder="Google API Key"/>
                 </div>
-                <button className="btn btn-lg btn-primary" type="submit">Authenticate</button>
+                <button
+                    className="btn btn-lg btn-primary"
+                    type="submit"
+                    onSubmit={this.handleSubmit}>
+                        Authenticate
+                </button>
             </form>
         );
+    },
+
+    handleSubmit: function(ev) {
+
+        ev.preventDefault();
+        var scopes = ['https://www.googleapis.com/auth/drive.readonly'];
+        var clientId = this.refs.clientId.getDOMNode().value.trim();
+        var apiKey = this.refs.apiKey.getDOMNode().value.trim();
+        gapi.client.setApiKey(apiKey);
+
+        function checkAuth() {
+            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true});
+        }
+
     }
 });
 
