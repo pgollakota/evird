60da5f4a671ac30c820f70f4a954004b54826e6d
Praveen Gollakota <praveen@chartio.com>
2014-10-05 23:16:26 -0700

    handleSubmit works with Google Client ID

--- evird/static/app/js/app.js
+++ evird/static/app/js/app.js
@@ -6,7 +6,7 @@ var GoogleApiAuthForm = React.createClass({
     },
     render: function () {
         return (
-            <form role="form">
+            <form role="form" onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label for="client_id">Client Id</label>
                     <input
@@ -17,20 +17,9 @@ var GoogleApiAuthForm = React.createClass({
                         name="client_id"
                         placeholder="Google API Client ID" />
                 </div>
-                <div className="form-group">
-                    <label for="api_key">API Key</label>
-                    <input
-                        ref="apiKey"
-                        id="api_key"
-                        className="form-control"
-                        type="text"
-                        name="api_key"
-                        placeholder="Google API Key"/>
-                </div>
                 <button
                     className="btn btn-lg btn-primary"
-                    type="submit"
-                    onSubmit={this.handleSubmit}>
+                    type="submit">
                         Authenticate
                 </button>
             </form>
@@ -42,11 +31,15 @@ var GoogleApiAuthForm = React.createClass({
         ev.preventDefault();
         var scopes = ['https://www.googleapis.com/auth/drive.readonly'];
         var clientId = this.refs.clientId.getDOMNode().value.trim();
-        var apiKey = this.refs.apiKey.getDOMNode().value.trim();
-        gapi.client.setApiKey(apiKey);
 
         function checkAuth() {
-            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true});
+            gapi.auth.authorize({client_id: clientId, scope: scopes},
+                handleAuthResult);
+        }
+        checkAuth()
+
+        function handleAuthResult(result) {
+            console.log(result);
         }
 
     }
