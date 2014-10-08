fa4189002a1d582a2b2069e480396fcf24f8d4d4
Praveen Gollakota <praveen@chartio.com>
2014-10-07 21:23:58 -0700

    Bootstrap template changes. Render App when gapi auth promise is fulfilled.

--- evird/static/app/js/app.js
+++ evird/static/app/js/app.js
@@ -3,28 +3,24 @@
 var GoogleApiAuthForm = React.createClass({
     render: function () {
         return (
-            <form role="form" onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                    <label for="client_id">Client Id</label>
-                    <input
-                        ref="clientId"
-                        id="client_id"
-                        className="form-control"
-                        type="text"
-                        name="client_id"
-                        placeholder="Google API Client ID" />
+            <div className="row">
+                <div className="col-md-6 col-md-offset-3">
+                    <form role="form" onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label for="client_id">Client id</label>
+                            <input ref="clientId" id="client_id" className="form-control"
+                                type="text" name="client_id" placeholder="Google API Client Id" />
+                        </div>
+                        <button className="btn btn-lg btn-primary" type="submit">
+                            Authenticate
+                        </button>
+                    </form>
                 </div>
-                <button
-                    className="btn btn-lg btn-primary"
-                    type="submit">
-                        Authenticate
-                </button>
-            </form>
+            </div>
         );
     },
 
     handleSubmit: function(ev) {
-
         ev.preventDefault();
         var scopes = ['https://www.googleapis.com/auth/drive.readonly'];
         var clientId = this.refs.clientId.getDOMNode().value.trim();
@@ -36,14 +32,18 @@ var GoogleApiAuthForm = React.createClass({
 
         function handleAuthResult(result) {
             // FIXME: Handle fail result
-            gapi.client.load('drive', 'v2').then(function() {console.log('loaded')});
-            React.renderComponent(<FilesList />, document.getElementById('app'));
+            gapi.client.load('drive', 'v2').then(function() {
+                React.renderComponent(<EvirdApp />, document.getElementById('app'));
+            });
         }
 
     }
 });
 
-var FilesList = React.createClass({
+var EvirdApp = React.createClass({
+    componentDidMount: function() {
+        gapi.client.drive.files.list().then(function(data) {console.log(data);})
+    },
     render: function () {
         return <div> Files here! </div>
     }
