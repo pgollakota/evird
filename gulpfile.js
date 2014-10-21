8b5692f4bba2f856eb672a16b723de66fb2e7f1e
Praveen Gollakota <praveen@chartio.com>
2014-10-21 09:48:27 -0700

    Remove vendor modules and let gulp take care of csss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
-var gulp = require('gulp');
 var browserify = require('browserify');
+var concat = require('gulp-concat');
 var del = require('del');
+var gulp = require('gulp');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 var paths = {
-  app_js: ['./evird/src/js/app.jsx'],
-  js: ['./evird/src/js/*.js']
+    app_js: ['./evird/src/js/app.jsx'],
+    js: ['./evird/src/js/*.js'],
+    css: [
+        './evird/src/css/main.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/font-awesome/css/font-awesome.css']
 };
 
-gulp.task('clean', function(done) {
-  del(['build'], done);
+gulp.task('js', function () {
+    browserify(paths.app_js)
+        .transform(reactify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./evird/static/js'));
 });
 
-gulp.task('js', ['clean'], function() {
-  browserify(paths.app_js)
-    .transform(reactify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./evird/static/js'));
+gulp.task('css', function () {
+    gulp.src(paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./evird/static/css/'))
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.js, ['js']);
+gulp.task('watch', function () {
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.js, ['js']);
 });
 
-gulp.task('default', ['watch', 'js']);
+gulp.task('default', ['watch', 'js', 'css']);
